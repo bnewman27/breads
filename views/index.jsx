1769ecb872ca4db112f77247e7357fe027add406
Ben Newman <108847653+bnewman27@users.noreply.github.com>
2022-11-17 20:33:22 -0800

    sending index and show data

--- views/index.jsx
+++ views/index.jsx
@@ -1,24 +1,25 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function Index ({breads})  {
+function Index ({breads, title})  {
     return (
-        <Default>
+        <Default title={title}>
         <h2>Index Page</h2>
         {/* <p>I have {breads[0].name} bread!</p> */}
         {/* This is a JSX comment. */}
         <ul>
         {
-        breads.map((bread, index)=> {
+            breads.map((bread, index)=> {
             return (
-            <li key={index}>
-                <a>
-                {bread.name}
-                </a>
-            </li>
-            )
-          })
-        }
+                <li key={index}>
+                    <a href={`/breads/${index}`}>
+                        {bread.name}
+                    </a>
+                </li>
+                    )
+                 }) 
+            }
+
 
 
         </ul>
