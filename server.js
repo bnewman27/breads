02c284343894432509a889b572ba9cb11b6e2ee9
Ben Newman <108847653+bnewman27@users.noreply.github.com>
2022-11-18 20:16:09 -0800

    create and new routes complete

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.urlencoded({extended: true}))
 
 
 // ROUTES
@@ -21,8 +22,11 @@ app.get('/', (req, res) => {
 // Breads
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
-  
-  
+
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
 
 // LISTEN
 app.listen(PORT, () => {
